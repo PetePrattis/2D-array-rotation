e838d5ba0a7efdcbc83708b4719c6078986927a3
Panagiotis Prattis <panagiotispr1997@gmail.com>
2019-10-07 20:13:16 +0300

    Update 8th_project.py

--- 8th_project.py
+++ 8th_project.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 
-#Author Παναγιώτης Πράττης-Panagiotis Prattis Π15120
-#This is the 8th exercise from the python exercise series for the class "ΕΙΣΑΓΩΓΗ ΣΤΗΝ ΕΠΙΣΤΗΜΗ ΤΩΝ ΥΠΟΛΟΓΙΣΤΩΝ - COMPUTER SCIENCE INTRODUCTION", University of Pireus
-#Project's demands:
-#Write a code which will read a file from a 8x8 table consisting of gaps (space) and 0. Then it displays the three rotations that it can have(by 90 degrees). Consider it like rotating a custom brick in Tetris.
+#Author Παναγιώτης Πράττης/Panagiotis Prattis
 
+'''
+A program which creates a 8x8 table consisting of gaps (space) and 0. 
+Then it displays the three rotations that it can have (by 90 degrees). 
+Consider it like rotating a custom brick in Tetris.
+'''
 
 import random
 
@@ -27,14 +29,9 @@ print('------------------')
 #        print table[i]
 
 
-
-
 # zip(*table) will swap axes of 2d arrays by stacking corresponding items from lists
 # into new lists. (The * operator tells the function to distribute the contained lists into arguments)
 # The [::-1] statement reverses array elements
-
-
-
 
 table2 = zip(*table[::-1])
 
